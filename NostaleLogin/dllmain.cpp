90f0b2071b9d7041ee554fe2bf40aae48a710e7a
Hatz <alextorres4101@gmail.com>
2022-07-04 11:57:06 +0200

    Added character selection

--- NostaleLogin/dllmain.cpp
+++ NostaleLogin/dllmain.cpp
@@ -1,22 +1,31 @@
-#include "NewServerSelectWidget.h"
+#include "GameStructures/TNTNewServerSelectWidget2.h"
+#include "GameStructures/TCharacterSelectWidget.h"
+#include <Windows.h>
 #include <string>
 
 int main()
 {
     const int BUFFER_SIZE = 2;
     const char* pipeName = "\\\\.\\pipe\\GflessClient";
-    int language, server, channel;
+    int language, server, channel, character;
     HANDLE hPipe;
     DWORD dwMode;
     BOOL fSuccess;
     std::string message;
     char readBuffer[BUFFER_SIZE];
-    NewServerSelectWidget newServerSelectWidget;
+    TNTNewServerSelectWidget2* newServerSelectWidget = nullptr;
+    TCharacterSelectWidget* characterSelectWidget = nullptr;
+
+    while (newServerSelectWidget == nullptr || characterSelectWidget == nullptr) 
+    {
+        newServerSelectWidget = TNTNewServerSelectWidget2::getInstance();
+        characterSelectWidget = TCharacterSelectWidget::getInstance();
+        Sleep(500);
+    }
 
     // Set up a pipe to communicate with the Gfless Client
     // and receive the values for the language server,
-    // the server and the channel
-
+    // the server, the channel and the character slot
     hPipe = CreateFileA(pipeName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
     if (hPipe == INVALID_HANDLE_VALUE)
@@ -74,20 +83,47 @@ int main()
 
     channel = readBuffer[0] - 0x30;
 
+    // Send the character message
+    message = "Character";
+    fSuccess = WriteFile(hPipe, message.c_str(), message.size(), NULL, NULL);
+
+    if (!fSuccess)
+        return EXIT_FAILURE;
+
+    ZeroMemory(readBuffer, BUFFER_SIZE);
+    fSuccess = ReadFile(hPipe, readBuffer, BUFFER_SIZE, NULL, NULL);
+
+    if (!fSuccess)
+        return EXIT_FAILURE;
+
+    character = readBuffer[0] - 0x30;
+    character = character - 1;
+
     // Wait for the login widget to be visible
     // and log into the desired server and channel
-    while (!newServerSelectWidget.isVisible()) 
+    while (!newServerSelectWidget->isVisible()) 
         Sleep(500);
 
-    newServerSelectWidget.selectLanguage(language);
+    newServerSelectWidget->selectLanguage(language);
     Sleep(3000);
 
-    while (!newServerSelectWidget.isVisible()) 
+    while (!newServerSelectWidget->isVisible()) 
         Sleep(500);
 
-    newServerSelectWidget.selectServer(server);
+    newServerSelectWidget->selectServer(server);
     Sleep(1000);
-    newServerSelectWidget.selectChannel(channel);;
+    newServerSelectWidget->selectChannel(channel);
+    Sleep(1000);
+
+    while (!characterSelectWidget->isVisible())
+        Sleep(500);
+
+    if (character >= 0)
+    {
+        characterSelectWidget->clickCharacterButton(character);
+        Sleep(1000);
+        characterSelectWidget->clickStartButton();
+    }
 
     return EXIT_SUCCESS;
 }
